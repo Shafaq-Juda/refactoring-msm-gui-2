d28c500951882c03f645f621cd5413f9b0e8330b
Your Name <you@example.com>
2024-02-19 22:58:15 +0000

    defined meta methods for Actor model as well

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -21,17 +21,19 @@ class Actor < ApplicationRecord
   #   return the_many
   # end
 
-  has_many(:characters, class_name="Character")
-  
-  def filmography
-    the_many = Array.new
+  has_many(:characters, class_name: "Character")
 
-    self.characters.each do |joining_record|
-      destination_record = joining_record.movie
+  # def filmography
+  #   the_many = Array.new
 
-      the_many.push(destination_record)
-    end
+  #   self.characters.each do |joining_record|
+  #     destination_record = joining_record.movie
 
-    return the_many
-  end
+  #     the_many.push(destination_record)
+  #   end
+
+  #   return the_many
+  # end
+
+  has_many(:filmography, :through => :characters, :source => :movie)  
 end
